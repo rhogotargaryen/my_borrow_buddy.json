df0e1d435b62aa39c4b14d49fd123368e201ba03
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-08-26 06:51:56 +0000

    made more refractoring into moslty static Item methods.  needs more work into protyping. but happy none the less.

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -12,38 +12,57 @@ class Item {
     
 }
 
-Item.make_index_link = (e) => {
+Item.display_liked_items = (e) => {
+    $.get(e.target.href, (data) => {
+        data.map((item) => {
+            var d = new Item(item)
+            $('#liked_items_container').append(`<p>you liked ${d.name}</p>`)
+            e.target.remove()
+        })
+    }, 'json')
+}
+
+Item.detach_form_present = 
+    () => {
+        if($('#detach_item_form')[0]){
+            $('#detach_item_form').click()
+        }
+    }
+
+Item.get_item_and_display = 
+    (e) => {
         $.get(`items/${e.target.parentElement.dataset.id}`, (data) => {
-         if($('#detach_item_form')[0]){
-             $('#detach_item_form').click()
-         }
-         var a = $('#link-wrap')
-         var b = $('a#create_item').detach()
-         $('#link-text-del').remove
-         $('#link-wrap').html(`</br>${data}`)
-         $('#main-content').prepend(b)
+            Item.detach_form_present
+            var a = $('#link-wrap')
+            var b = $('a#create_item').detach()
+            $('#link-text-del').remove
+            $('#link-wrap').html(`</br>${data}`)
+            $('#main-content').prepend(b)
         })             
     }
+
+Item.get_items_and_mlinks = (e) => {
+    $.get('/items', e.target.dataset, (data) => {
+        var template = Handlebars.compile($('#item_index_template').html())
+        var added_items = template(data)
+        $('#index-click').detach()
+        $('#index-item-container').prepend(added_items)
+    }, 'json')
+}
 
 $(function() {
     
     $('#index-item-container').click((e) => {
         e.preventDefault()
         if(e.target.tagName == "STRONG") {
-            Item.make_index_link(e)
+            Item.get_item_and_display(e)
         }
     })   
     
     $("#index-click").on('click', (e) => {
-        $.get('/items', e.target.dataset, (data) => {
-            var template = Handlebars.compile($('#item_index_template').html())
-            var added_items = template(data)
-            $('#index-click').detach()
-            $('#index-item-container').prepend(added_items)
-        }, 'json').done(() => {
-         
-        })
+        Item.get_items_and_mlinks(e)
     })
+    // need to work on refractoring this method a but more also not so much protypes are being used rather than statics
     $('#create_item').on('click', (e) => {
         e.preventDefault()
         $.get('/items/new', (data) => {
@@ -53,7 +72,6 @@ $(function() {
             $('#main-content').prepend('<a href="#/" class="navlink" id="detach_item_form">detach new item form</a>')
             $('#new_item_form_container').on('submit', a, function(e) {
                 e.preventDefault()
-                var template = Handlebars.compile($('#item_template').html())
                 $.post('/items', $('form#new_item').serialize(), (data) => {
                     var b = new Item(data)
                     var added_item = b.new_item_template()
@@ -76,14 +94,7 @@ $(function() {
     })
     $('#view_liked_items').on('click', (e) => {
         e.preventDefault()
-        $.get(e.target.href, (data) => {
-            data.map((item) => {
-                var d = new Item(item)
-                $('#liked_items_container').append(`<p>you liked ${d.name}</p>`)
-                e.target.remove()
-            })
-        }, 'json')
+        Item.display_liked_items(e)
     })
 })
-
 
